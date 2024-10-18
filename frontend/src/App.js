4b6d710bad98ba99f9f0ceab52e6f6c2d36e3a89
Selina Lin <linselin2828@gmail.com>
2024-10-18 01:35:07 -0400

    successfully added navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import squirrel from './assets/squirrel.png';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <img src={squirrel} className="Squirrel-picture" alt="squirrel" />
         <h1> Hello World! </h1>
         <p> This is going to be my database application project. </p>
       </header>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import './Navbar.css';
+import '../styles/navbar.css';
+import squirrel from '../assets/squirrel.png';
 
 const Navbar = () => {
   return (
 
 <nav className="navbar">
-  {/* Navbar content will go here */}
+  <div className="navbar-left">
+    <a href="/" className="logo">
+      Aviary Ailment Appointments
+    </a>
+  </div>
+  <div className="navbar-center">
+    <ul className="nav-links">
+      <li>
+        <a href="/appointments">Schedule Appointments</a>
+      </li>
+      <li>
+        <a href="/patient-owner-info">Patient & Owner Registration</a>
+      </li>
+      <li>
+        <a href="/data">Data Analysis</a>
+      </li>
+    </ul>
+  </div>
+  <div className="navbar-right">
+    <img src={squirrel} className="Squirrel-picture" alt="squirrel" />
+  </div>
 </nav>
 );
 };
