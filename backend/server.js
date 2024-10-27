598a0edd05cc8b6ac30d9694693f5f9c7ca2b42c
Selina Lin <linselin2828@gmail.com>
2024-10-27 15:45:32 -0400

    added patients

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import cors from "cors";
-import owner from "./routes/owner.js";
+import owners from "./routes/owner.js";
+import patients from "./routes/patient.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/owner", owner);
+app.use("/owner", owners);
+app.use("/patient", patients);
 
 // start the Express server
 app.listen(PORT, () => {
